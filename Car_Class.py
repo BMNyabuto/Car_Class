fa0217380152e06e8051314784addee00d084444
boman <bmnyabuto@gmail.com>
2017-01-18 23:22:17 +0300

    Added a method that determines the number of wheels the instance of the car provided has

--- Car_Class.py
+++ Car_Class.py
@@ -7,16 +7,25 @@ class Car(object):
 		self.model = model
 		self.car_type = car_type
     
-def num_of_doors(self):
-	if self.name is 'Koenigsegg' :
-		self.No_of_doors = 2
-		return self.No_of_doors
+	def num_of_doors(self):
+		if self.name is 'Koenigsegg' :
+			self.No_of_doors = 2
+			return self.No_of_doors
 
-	elif self.name is 'Porsche':
-		self.No_of_doors = 2
-		return self.No_of_doors
+		elif self.name is 'Porsche':
+			self.No_of_doors = 2
+			return self.No_of_doors
 
-	else:
-		self.No_of_doors = 4
-		return self.No_of_doors
-	
+		else:
+			self.No_of_doors = 4
+			return self.No_of_doors
+	def num_of_wheels(self):	
+		
+		if self.car_type == 'trailer':	
+			wheels = 8
+			return wheels
+			
+		else:	
+			wheels = 4
+			return  wheels
+		
