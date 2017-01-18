00bc83c977905a56bf22c1518924e81a48fdf69e
boman <bmnyabuto@gmail.com>
2017-01-18 23:17:02 +0300

    Added class variables and the initialisation method(AKA construct in other environments)

--- Car_Class.py
+++ Car_Class.py
@@ -7,34 +7,3 @@ class Car(object):
 		self.model = model
 		self.car_type = car_type
     
-    def num_of_doors(self):
-		
-		if self.name is 'Koenigsegg' :
-			self.No_of_doors = 2
-			return self.No_of_doors
-		elif self.name is 'Porsche':
-			self.No_of_doors = 2
-			return self.No_of_doors
-			
-		else:
-			self.No_of_doors = 4
-			return self.No_of_doors
-	
-	def num_of_wheels(self):	
-		
-		if self.car_type == 'trailer':	
-			wheels = 8
-			return wheels
-			
-		else:	
-			wheels = 4
-			return  wheels
-		
-	def drive(self, x):
-		
-		if self.car_type == 'trailer':
-			self.speed = 77
-			return self.speed
-		else:
-			self.speed = int(math.pow(x,3))
-			return self.speed
