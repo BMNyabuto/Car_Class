b0f46b9c00c217060a6ce6d4fe1dfbe3fc1ac048
boman <bmnyabuto@gmail.com>
2017-01-18 23:20:05 +0300

    Added a method that determines the number of doors the instance of the car provided has

--- Car_Class.py
+++ Car_Class.py
@@ -7,3 +7,16 @@ class Car(object):
 		self.model = model
 		self.car_type = car_type
     
+def num_of_doors(self):
+	if self.name is 'Koenigsegg' :
+		self.No_of_doors = 2
+		return self.No_of_doors
+
+	elif self.name is 'Porsche':
+		self.No_of_doors = 2
+		return self.No_of_doors
+
+	else:
+		self.No_of_doors = 4
+		return self.No_of_doors
+	
